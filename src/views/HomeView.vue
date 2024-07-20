596dbc61c5ce5e09881f703574960e2e4178c7bf
mafiapatch <g8071107@gmail.com>
2024-07-20 22:35:21 +0800

    [modify] progress backup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -60,56 +60,112 @@ export default {
             <div id="story_carousel_title">
               <img src="/assets/img/story_title.png" alt="title" />
             </div>
-            <div id="story_carousel" class="carousel slide" data-ride="carousel">
+            <div
+              id="story_carousel"
+              class="carousel slide"
+              data-ride="carousel"
+            >
               <div class="carousel-inner">
                 <div class="carousel-item active">
                   <div class="row">
                     <div class="col-6 col-story">
                       <div class="story">
-                        <img src="https://picsum.photos/537/352" class="story-img" alt="..." />
+                        <img
+                          src="https://picsum.photos/537/352"
+                          class="story-img"
+                          alt="..."
+                        />
                         <div class="story-body">
                           <h5 class="story-title">陪伴我們全家的最佳神隊友</h5>
-                          <p class="story-content">人生第一台車就獻給Custin！全家居住於台中，隻身一人工作於台北的遲先生，為了陪伴老婆與孩子，每周末都會開著Custin帶家人們出遊，享受甜蜜的家庭時光。</p>
+                          <p class="story-content">
+                            人生第一台車就獻給Custin！全家居住於台中，隻身一人工作於台北的遲先生，為了陪伴老婆與孩子，每周末都會開著Custin帶家人們出遊，享受甜蜜的家庭時光。
+                          </p>
                         </div>
                       </div>
                     </div>
                     <div class="col-6 col-story">
                       <div class="story">
-                        <img src="https://picsum.photos/537/352" class="story-img" alt="..." />
+                        <img
+                          src="https://picsum.photos/537/352"
+                          class="story-img"
+                          alt="..."
+                        />
                         <div class="story-body">
-                          <h5 class="story-title">七人座MPV Custin A款交車，滿意!!</h5>
-                          <p class="story-content">Custin就是載家人出遊、偶爾滿載、臨時載貨，至於1.5T渦輪引擎開這麼大台的車到底夠不夠力呢?一踩大扭力就出來，使用到目前覺得滿載也不會感受到重拖，順順開來個瞬間加速也夠超車，幾天下來目前是滿意的!</p>
+                          <h5 class="story-title">
+                            七人座MPV Custin A款交車，滿意!!
+                          </h5>
+                          <p class="story-content">
+                            Custin就是載家人出遊、偶爾滿載、臨時載貨，至於1.5T渦輪引擎開這麼大台的車到底夠不夠力呢?一踩大扭力就出來，使用到目前覺得滿載也不會感受到重拖，順順開來個瞬間加速也夠超車，幾天下來目前是滿意的!
+                          </p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-
+                <div class="carousel-item">
+                  <div class="row">
+                    <div class="col-6 col-story">
+                      <div class="story">
+                        <img
+                          src="https://picsum.photos/537/352"
+                          class="story-img"
+                          alt="..."
+                        />
+                        <div class="story-body">
+                          <h5 class="story-title">1陪伴我們全家的最佳神隊友</h5>
+                          <p class="story-content">
+                            人生第一台車就獻給Custin！全家居住於台中，隻身一人工作於台北的遲先生，為了陪伴老婆與孩子，每周末都會開著Custin帶家人們出遊，享受甜蜜的家庭時光。
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-6 col-story">
+                      <div class="story">
+                        <img
+                          src="https://picsum.photos/537/352"
+                          class="story-img"
+                          alt="..."
+                        />
+                        <div class="story-body">
+                          <h5 class="story-title">
+                            七人座MPV Custin A款交車，滿意!!
+                          </h5>
+                          <p class="story-content">
+                            Custin就是載家人出遊、偶爾滿載、臨時載貨，至於1.5T渦輪引擎開這麼大台的車到底夠不夠力呢?一踩大扭力就出來，使用到目前覺得滿載也不會感受到重拖，順順開來個瞬間加速也夠超車，幾天下來目前是滿意的!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
               <a
                 class="carousel-control-prev"
-                href="#story_carousel"
+                data-bs-target="#story_carousel"
                 role="button"
-                data-slide="prev"
+                data-bs-slide="prev"
               >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
+                <img src="/assets/img/story_arrow_left.png" />
               </a>
               <a
                 class="carousel-control-next"
-                href="#story_carousel"
+                data-bs-target="#story_carousel"
                 role="button"
-                data-slide="next"
+                data-bs-slide="next"
               >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
+                <img src="/assets/img/story_arrow_right.png" />
               </a>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="story_wall">
+      <div class="container">
+        <div class="row">
+          <div class="col-6 story-wall left">123</div>
+          <div class="col-6 story-wall right">123</div>
         </div>
       </div>
     </section>
