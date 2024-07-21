6a1eb7731e215389d0f1b40ce4dcd68d14ccc979
mafiapatch <g8071107@gmail.com>
2024-07-21 18:51:14 +0800

    [modify] progress backup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -228,6 +228,84 @@ export default {
       </div>
     </section>
 
+    <section id="story_carousel_mobile">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div id="story_carousel_title">
+              <img src="/assets/img/story_title_mobile.png" alt="title" />
+            </div>
+            <div
+              id="story_carousel"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="row">
+                    <div class="col-12 col-story">
+                      <div class="story">
+                        <img
+                          src="https://picsum.photos/537/352"
+                          class="story-img"
+                          alt="..."
+                        />
+                        <div class="story-body">
+                          <h5 class="story-title">
+                            七人座MPV Custin A款交車，滿意!!
+                          </h5>
+                          <p class="story-content">
+                            Custin就是載家人出遊、偶爾滿載、臨時載貨，至於1.5T渦輪引擎開這麼大台的車到底夠不夠力呢?一踩大扭力就出來，使用到目前覺得滿載也不會感受到重拖
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="row">
+                    <div class="col-12 col-story">
+                      <div class="story">
+                        <img
+                          src="https://picsum.photos/537/352"
+                          class="story-img"
+                          alt="..."
+                        />
+                        <div class="story-body">
+                          <h5 class="story-title">
+                            七人座MPV Custin A款交車，滿意!!
+                          </h5>
+                          <p class="story-content">
+                            Custin就是載家人出遊、偶爾滿載、臨時載貨，至於1.5T渦輪引擎開這麼大台的車到底夠不夠力呢?一踩大扭力就出來，使用到目前覺得滿載也不會感受到重拖，順順開來個瞬間加速也夠超車，幾天下來目前是滿意的!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a
+                class="carousel-control-prev"
+                data-bs-target="#story_carousel"
+                role="button"
+                data-bs-slide="prev"
+              >
+                <img src="/assets/img/story_arrow_left.png" />
+              </a>
+              <a
+                class="carousel-control-next"
+                data-bs-target="#story_carousel"
+                role="button"
+                data-bs-slide="next"
+              >
+                <img src="/assets/img/story_arrow_right.png" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section id="story_wall_pc">
       <div class="container">
         <div class="row">
