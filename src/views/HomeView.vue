20272171b71cb5ce0ede126b0582046b2e20f1e4
mafiapatch <g8071107@gmail.com>
2025-02-13 19:23:27 +0800

    [modify] change text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -92,18 +92,14 @@ export default {
       };
     };
     const animateNum = () => {
-      const targetNumber = 10000;
+      const targetNumber = 11565;
       const duration = 5000;
       const stepTime = Math.abs(Math.floor(duration / targetNumber));
       const slowDownPoint = 9000;
       const slowDownFactor = 1;
 
       const timer = setInterval(() => {
-      if (animatedNumber.value < slowDownPoint) {
-        animatedNumber.value += 30;
-      } else {
         animatedNumber.value += slowDownFactor;
-      }
 
       if (animatedNumber.value >= targetNumber) {
         animatedNumber.value = targetNumber;
@@ -428,7 +424,7 @@ export default {
             <div class="offcanvas-body">
               <div class="menu-list" @click="scrollTo('page_intro_mobile')">故事募集中</div>
               <div class="menu-list" @click="scrollTo('story_carousel_mobile')">幸福故事集</div>
-              <div class="menu-list" @click="scrollTo('story_share_mobile')">名人車主故事&體驗分享</div>
+              <div class="menu-list" @click="scrollTo('story_share_mobile')">Youtube車主故事&體驗分享</div>
             </div>
           </div>
         </div>
@@ -451,7 +447,7 @@ export default {
           <div class="col-12 text-center">
             <div class="link" @click="scrollTo('page_intro_pc')">故事募集中</div>
             <div class="link" @click="scrollTo('story_carousel_pc')">幸福故事集</div>
-            <div class="link" @click="scrollTo('story_share_pc')">名人車主故事&體驗分享</div>
+            <div class="link" @click="scrollTo('story_share_pc')">Youtube車主故事&體驗分享</div>
           </div>
         </div>
       </div>
@@ -493,7 +489,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '照顧全家人的貼心夥伴',
+                          '三重吳先生｜照顧全家人的貼心夥伴',
                           `<img src='/assets/img/story/a/1.webp' loading='lazy' class='story-img' alt='...'/><br>我第一眼就被CUSTIN的顏值外觀給吸引，流線型的車身、大器的車頭霸氣設計以及尾燈也很醒目，都是目前流行的設計元素。整體給我的感覺就是很前衛有型。我也很喜歡它的駕駛座，直立的主機螢幕加上按鍵式排檔，使用上非常直覺便利。<br>
                             <img src='/assets/img/story/a/2.webp' loading='lazy' class='story-img' alt='...'/><br>
                           但內裝的部分才是讓我決定成為車主的最大原因，VIP皇家座椅第二排座椅、電動滑門、超大雙天窗，真的讓我出乎意料，CUSTIN通通都有，重點是第二排座椅竟然還有通風加熱功能，配備給的實在不手軟！<br>
@@ -512,7 +508,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            照顧全家人的貼心夥伴
+                            三重吳先生｜照顧全家人的貼心夥伴
                           </h5>
                           <p class="story-content">
                             我第一眼就被CUSTIN的顏值外觀給吸引，流線型的車身、大器的車頭霸氣設計以及尾燈也很醒目，都是目前流行的設計元素。整體給我的感覺就是很前衛有型...
@@ -526,7 +522,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '配備給好給滿超有料',
+                          '永和周先生 ｜配備給好給滿超有料',
                           `<img src='/assets/img/story/b/1.webp' class='story-img' loading='lazy' alt='...'/><br><p>我們家的兩個小朋友，最喜歡的就是CUSTIN的電動側滑門，因為剛換車的時候，我跟他們開了一個小玩笑，讓他們站在車門前大喊「魔法開開」，我在一旁偷偷用鑰匙遙控開啟，他們看著滑門慢慢滑開興奮得驚呼連連。</p>
                           <img src='/assets/img/story/b/2.webp' class='story-img' loading='lazy' alt='...'/>
                           <p>後來當然還是知道了這個咒語的秘密，但每當我們準備出發，小朋友們還是會興奮地跑到車前高喊「魔法開開」，然後看著車門自動打開，再開心地跳上車，我跟老婆都會心一笑。這個習慣成為了我們每次出遊前的小小儀式，這種互動不僅增加了樂趣，也讓孩子們更加期待每一次的旅行。</p>
@@ -544,7 +540,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            配備給好給滿超有料
+                            永和周先生 ｜配備給好給滿超有料
                           </h5>
                           <p class="story-content">
                             我們家的兩個小朋友，最喜歡的就是CUSTIN的電動側滑門，因為剛換車的時候，我跟他們開了一個小玩笑，讓他們站在車門前大喊「魔法開開」，我在一旁偷偷用鑰匙遙控開啟，他們看著滑門慢慢滑開興奮得驚呼連連。
@@ -563,7 +559,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '替每一位著想的家庭神車',
+                          '大安李先生 ｜體貼每一位的家庭神車',
                           `<img src='/assets/img/story/c/1.webp' class='story-img' loading='lazy' alt='...'/><br><p>當初是因為有空間上的需求，才開始找尋7人座的車，中間陸陸續續有看了很多款，還包含5+2的休旅車，但在都試乘過一遍後，只有CUSTIN在空間上能夠滿足我，而且也讓我印象最深刻！</p>
                           <img src='/assets/img/story/c/2.webp' class='story-img' loading='lazy' alt='...'/><br>
                           <p>當初試過後就被他的豐富配備給吸引，尤其是第二排座椅跟電動滑門，不敢相信在這個價位可以做到這樣。除此之外車內也有很多貼心的小細節，比如說第二排遮陽簾、折疊桌板，甚至還有杯架跟平板架，就像是在搭飛機一樣，就連第三排也有獨立的冷氣出風口跟USB充電孔，小朋友在後面也坐的很舒服。</p>
@@ -581,7 +577,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            替每一位著想的家庭神車
+                            大安李先生 ｜體貼每一位的家庭神車
                           </h5>
                           <p class="story-content">
                             當初是因為有空間上的需求，才開始找尋7人座的車，中間陸陸續續有看了很多款，還包含5+2的休旅車，但在都試乘過一遍後，只有CUSTIN在空間上能夠滿足我，而且也讓我印象最深刻！
@@ -596,7 +592,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '每一次旅程都無比享受',
+                          '新店許先生｜每一次旅程都無比享受',
                           `<img src='/assets/img/story/d/1.webp' class='story-img' loading='lazy' alt='...'/><br>
                           <p>我爸媽上了年紀後，行動變得沒那麼靈活，原本的車子對他們來說上下車不太方便，坐久了也會有身體痠痛的問題，因緣際會之下看到了CUSTIN，便帶著他們一起去看車。</p>
                           <img src='/assets/img/story/d/2.webp' class='story-img' loading='lazy' alt='...'/><br>
@@ -618,7 +614,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            每一次旅程都無比享受
+                            新店許先生｜每一次旅程都無比享受
                           </h5>
                           <p class="story-content">
                             我爸媽上了年紀後，行動變得沒那麼靈活，原本的車子對他們來說上下車不太方便，坐久了也會有身體痠痛的問題，因緣際會之下看到了CUSTIN，便帶著他們一起去看車。
@@ -637,7 +633,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '寬敞大空間 輕鬆出遊沒煩惱',
+                          '信義劉小姐｜寬敞空間輕鬆出遊',
                           `<img src='/assets/img/story/e/1.webp' class='story-img' loading='lazy' alt='...'/><br><p>我們家小朋友有在學跳舞，有一次在路上剛好聽到一首他們前陣子剛學過的音樂，他們很興奮的說這是上禮拜老師教的，然後他們就在後面直接跳起來～</p>
                           <img src='/assets/img/story/e/2.webp' class='story-img' loading='lazy' alt='...'/><br>
                           <p>CUSTIN的空間真的很大，每個位置都可以都坐得很舒服，有時候出去玩會帶多一些行李或其他東西，也都綽綽有餘，而且行李箱是電動尾門，上下東西很方便。</p>
@@ -658,7 +654,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            寬敞大空間 輕鬆出遊沒煩惱
+                            信義劉小姐｜寬敞空間輕鬆出遊
                           </h5>
                           <p class="story-content">
                             我們家小朋友有在學跳舞，有一次在路上剛好聽到一首他們前陣子剛學過的音樂，他們很興奮的說這是上禮拜老師教的，然後他們就在後面直接跳起來～
@@ -715,7 +711,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '照顧全家人的貼心夥伴',
+                          '三重吳先生｜照顧全家人的貼心夥伴',
                           `<img src='/assets/img/story/a/1.webp' loading='lazy' class='story-img' alt='...'/><br>我第一眼就被CUSTIN的顏值外觀給吸引，流線型的車身、大器的車頭霸氣設計以及尾燈也很醒目，都是目前流行的設計元素。整體給我的感覺就是很前衛有型。我也很喜歡它的駕駛座，直立的主機螢幕加上按鍵式排檔，使用上非常直覺便利。<br>
                             <img src='/assets/img/story/a/2.webp' loading='lazy' class='story-img' alt='...'/><br>
                           但內裝的部分才是讓我決定成為車主的最大原因，VIP皇家座椅第二排座椅、電動滑門、超大雙天窗，真的讓我出乎意料，CUSTIN通通都有，重點是第二排座椅竟然還有通風加熱功能，配備給的實在不手軟！<br>
@@ -734,7 +730,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            照顧全家人的貼心夥伴
+                            三重吳先生｜照顧全家人的貼心夥伴
                           </h5>
                           <p class="story-content">
                             我第一眼就被CUSTIN的顏值外觀給吸引，流線型的車身、大器的車頭霸氣設計以及尾燈也很醒目，都是目前流行的設計元素。整體給我的感覺就是很前衛有型。...
@@ -752,7 +748,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '配備給好給滿超有料',
+                          '永和周先生 ｜配備給好給滿超有料',
                           `<img src='/assets/img/story/b/1.webp' loading='lazy' class='story-img' alt='...'/><br><p>我們家的兩個小朋友，最喜歡的就是CUSTIN的電動側滑門，因為剛換車的時候，我跟他們開了一個小玩笑，讓他們站在車門前大喊「魔法開開」，我在一旁偷偷用鑰匙遙控開啟，他們看著滑門慢慢滑開興奮得驚呼連連。</p>
                           <img src='/assets/img/story/b/2.webp' loading='lazy' class='story-img' alt='...'/>
                           <p>後來當然還是知道了這個咒語的秘密，但每當我們準備出發，小朋友們還是會興奮地跑到車前高喊「魔法開開」，然後看著車門自動打開，再開心地跳上車，我跟老婆都會心一笑。這個習慣成為了我們每次出遊前的小小儀式，這種互動不僅增加了樂趣，也讓孩子們更加期待每一次的旅行。</p>
@@ -771,7 +767,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            配備給好給滿超有料
+                            永和周先生 ｜配備給好給滿超有料
                           </h5>
                           <p class="story-content">
                             我們家的兩個小朋友，最喜歡的就是CUSTIN的電動側滑門，因為剛換車的時候，我跟他們開了一個小玩笑，讓他們站在車門前大喊「魔法開開」，我在一旁偷偷用鑰匙遙控開啟，他們看著滑門慢慢滑開興奮得驚呼連連。
@@ -791,7 +787,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '替每一位著想的家庭神車',
+                          '大安李先生 ｜體貼每一位的家庭神車',
                           `<img src='/assets/img/story/c/1.webp' loading='lazy' class='story-img' alt='...'/><br><p>當初是因為有空間上的需求，才開始找尋7人座的車，中間陸陸續續有看了很多款，還包含5+2的休旅車，但在都試乘過一遍後，只有CUSTIN在空間上能夠滿足我，而且也讓我印象最深刻！</p>
                           <img src='/assets/img/story/c/2.webp' loading='lazy' class='story-img' alt='...'/><br>
                           <p>當初試過後就被他的豐富配備給吸引，尤其是第二排座椅跟電動滑門，不敢相信在這個價位可以做到這樣。除此之外車內也有很多貼心的小細節，比如說第二排遮陽簾、折疊桌板，甚至還有杯架跟平板架，就像是在搭飛機一樣，就連第三排也有獨立的冷氣出風口跟USB充電孔，小朋友在後面也坐的很舒服。</p>
@@ -809,7 +805,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            替每一位著想的家庭神車
+                            大安李先生 ｜體貼每一位的家庭神車
                           </h5>
                           <p class="story-content">
                             當初是因為有空間上的需求，才開始找尋7人座的車，中間陸陸續續有看了很多款，還包含5+2的休旅車，但在都試乘過一遍後，只有CUSTIN在空間上能夠滿足我，而且也讓我印象最深刻！
@@ -827,7 +823,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '每一次旅程都無比享受',
+                          '新店許先生｜每一次旅程都無比享受',
                           `<img src='/assets/img/story/d/1.webp' loading='lazy' class='story-img' alt='...'/><br>
                           <p>我爸媽上了年紀後，行動變得沒那麼靈活，原本的車子對他們來說上下車不太方便，坐久了也會有身體痠痛的問題，因緣際會之下看到了CUSTIN，便帶著他們一起去看車。</p>
                           <img src='/assets/img/story/d/2.webp' loading='lazy' class='story-img' alt='...'/><br>
@@ -849,7 +845,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            每一次旅程都無比享受
+                            新店許先生｜每一次旅程都無比享受
                           </h5>
                           <p class="story-content">
                             我爸媽上了年紀後，行動變得沒那麼靈活，原本的車子對他們來說上下車不太方便，坐久了也會有身體痠痛的問題，因緣際會之下看到了CUSTIN，便帶著他們一起去看車。
@@ -868,7 +864,7 @@ export default {
                       data-bs-target="#storyModal"
                       @click="
                         storyModalSetData(
-                          '寬敞大空間 輕鬆出遊沒煩惱',
+                          '信義劉小姐｜寬敞空間輕鬆出遊',
                           `<img src='/assets/img/story/e/1.webp' loading='lazy' class='story-img' alt='...'/><br><p>我們家小朋友有在學跳舞，有一次在路上剛好聽到一首他們前陣子剛學過的音樂，他們很興奮的說這是上禮拜老師教的，然後他們就在後面直接跳起來～</p>
                           <img src='/assets/img/story/e/2.webp' loading='lazy' class='story-img' alt='...'/><br>
                           <p>CUSTIN的空間真的很大，每個位置都可以都坐得很舒服，有時候出去玩會帶多一些行李或其他東西，也都綽綽有餘，而且行李箱是電動尾門，上下東西很方便。</p>
@@ -888,7 +884,7 @@ export default {
                         />
                         <div class="story-body">
                           <h5 class="story-title">
-                            寬敞大空間 輕鬆出遊沒煩惱
+                            信義劉小姐｜寬敞空間輕鬆出遊
                           </h5>
                           <p class="story-content">
                             我們家小朋友有在學跳舞，有一次在路上剛好聽到一首他們前陣子剛學過的音樂，他們很興奮的說這是上禮拜老師教的，然後他們就在後面直接跳起來～
